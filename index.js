58163143ecbf110d979553639719de4bb0a896ea
Fabiano Cunha <cunhasb@aol.com>
2017-11-27 13:17:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
+// function getFirstSelector(selector){
+//   return document.querySelector(selector)
+// }
+//
+// function nestedTarget(){
+//   return document.querySelector("#nested .target")
+// }
+//
+// function increaseRankBy(n){
+//   var elements = document.querySelectorAll(".ranked-list li ")
+//   for (let i=0; i<elements.length;i++){
+//     elements[i].innerHTML= parseInt(elements[i].innerHTML) + n
+//   }
+// }
+//
+// function deepestChild(){
+//   var current=document.querySelectorAll("#grand-node")[0].lastElementChild
+//   var last=[]
+//   while (current){
+//     last= current
+//     current = current.lastElementChild
+//   }
+//   return last
+// }
+
 function getFirstSelector(selector){
-  return document.querySelector(selector)
+  return document.querySelector(selector);
 }
 
 function nestedTarget(){
-  return document.querySelector("#nested .target")
-}
-
-function increaseRankBy(n){
-  var elements = document.querySelectorAll(".ranked-list li ")
-  for (let i=0; i<elements.length;i++){
-    elements[i].innerHTML= parseInt(elements[i].innerHTML) + n
-  }
+  return document.querySelector('#nested .target')
 }
 
 function deepestChild(){
-  var current=document.querySelectorAll("#grand-node")[0].lastElementChild
-  var last=[]
+  let current = document.querySelectorAll('#grand-node div')[0].lastElementChild;
+  let last = "";
   while (current){
-    last= current
-    current = current.lastElementChild
+    last = current;
+    current = current.lastElementChild;
   }
-  return last
+  return last;
+}
+
+function increaseRankBy(n){
+  let current = document.querySelectorAll('.ranked-list li');
+  for (let i = 0; i < current.length; i++){
+    current[i].innerHTML= parseInt(current[i].innerHTML)  + n;
+  }
 }
